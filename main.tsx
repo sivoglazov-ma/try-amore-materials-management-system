f0b633ebf496c1c78984f06fd0803917fcabb29f
sivoglazov-ma <sivoglazov.m@gmail.com>
2025-11-04 19:59:32 +0300

    change path to basic-styles.css
    change path to basic-styles.css

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './basic-styles.css'
+import './styles/basic-styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
