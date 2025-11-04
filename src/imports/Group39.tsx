9e0a6a9b0e6a1690fb5561177b5e477647e3e5a5
sivoglazov-ma <sivoglazov.m@gmail.com>
2025-11-05 02:13:04 +0300

    Add files via upload

---
+++ src/imports/Group39.tsx
@@ -0,0 +1,203 @@
+import svgPaths from "./svg-isb758qp2k";
+
+function Group27() {
+  return (
+    <div className="absolute contents left-[30px] top-[347px]">
+      <div className="absolute bg-[#121212] h-[48px] left-[30px] rounded-[11px] top-[347px] w-[540px]">
+        <div aria-hidden="true" className="absolute border border-[#292929] border-solid inset-0 pointer-events-none rounded-[11px]" />
+      </div>
+      <p className="absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[51px] not-italic text-[#a5a5a5] text-[17px] top-[361px] w-[467px]">https://aromausa.ru/fragrance/tproduct/3977123...</p>
+    </div>
+  );
+}
+
+function Group34() {
+  return (
+    <div className="absolute contents left-[30px] top-[317px]">
+      <Group27 />
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[normal] left-[30px] not-italic text-[17px] text-nowrap text-white top-[317px] whitespace-pre">Ссылка на магазин</p>
+    </div>
+  );
+}
+
+function Group30() {
+  return (
+    <div className="absolute contents left-[30px] top-[455px]">
+      <div className="absolute bg-[#121212] h-[48px] left-[30px] rounded-[11px] top-[455px] w-[540px]">
+        <div aria-hidden="true" className="absolute border border-[#292929] border-solid inset-0 pointer-events-none rounded-[11px]" />
+      </div>
+      <p className="absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[51px] not-italic text-[#a5a5a5] text-[17px] top-[469px] w-[467px]">aromausa.ru</p>
+    </div>
+  );
+}
+
+function Group33() {
+  return (
+    <div className="absolute contents left-[30px] top-[420px]">
+      <Group30 />
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[normal] left-[30px] not-italic text-[17px] text-nowrap text-white top-[420px] whitespace-pre">Поставщик</p>
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="absolute inset-[11.45%_15.63%_11.46%_15.63%]" data-name="Group">
+      <div className="absolute inset-[-1.62%_-1.82%]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 29 32">
+          <g id="Group">
+            <path d={svgPaths.p16350e80} id="Vector" stroke="var(--stroke-0, #A5A5A5)" strokeLinecap="round" strokeLinejoin="round" />
+            <path d={svgPaths.pff5dc00} id="Vector_2" stroke="var(--stroke-0, #A5A5A5)" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M14.25 13.4167V24.7067" id="Vector_3" stroke="var(--stroke-0, #A5A5A5)" strokeLinecap="round" strokeMiterlimit="10" />
+            <path d={svgPaths.p1304080} id="Vector_4" stroke="var(--stroke-0, #A5A5A5)" strokeLinecap="round" strokeLinejoin="round" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Group31() {
+  return (
+    <div className="absolute contents left-[30px] top-[562px]">
+      <div className="absolute bg-[#121212] h-[100.72px] left-[30px] rounded-[11px] top-[562px] w-[540px]">
+        <div aria-hidden="true" className="absolute border border-[#a5a5a5] border-dashed inset-0 pointer-events-none rounded-[11px]" />
+      </div>
+      <div className="absolute left-[280px] size-[40px] top-[579px]" data-name="mage:file-upload">
+        <Group />
+      </div>
+      <p className="absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[calc(50%+0.5px)] not-italic text-[#a5a5a5] text-[17px] text-center top-[623px] translate-x-[-50%] w-[203px]">Загрузить файл</p>
+    </div>
+  );
+}
+
+function Group32() {
+  return (
+    <div className="absolute contents left-[30px] top-[527px]">
+      <Group31 />
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[normal] left-[30px] not-italic text-[17px] text-nowrap text-white top-[527px] whitespace-pre">Фото расходника</p>
+    </div>
+  );
+}
+
+function Group12() {
+  return (
+    <div className="absolute contents left-[30px] top-[140px]">
+      <div className="absolute bg-[#121212] h-[48px] left-[30px] rounded-[11px] top-[140px] w-[540px]">
+        <div aria-hidden="true" className="absolute border border-[#292929] border-solid inset-0 pointer-events-none rounded-[11px]" />
+      </div>
+      <p className="absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[51px] not-italic text-[#a5a5a5] text-[17px] top-[154px] w-[467px]">Масло Роза и кардамон</p>
+    </div>
+  );
+}
+
+function Group36() {
+  return (
+    <div className="absolute contents left-[30px] top-[105px]">
+      <Group12 />
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[normal] left-[30px] not-italic text-[17px] text-nowrap text-white top-[105px] whitespace-pre">Наименование</p>
+    </div>
+  );
+}
+
+function Group37() {
+  return (
+    <div className="absolute contents leading-[normal] left-[30px] not-italic top-[29px]">
+      <p className="absolute font-['Inter:Regular',sans-serif] font-normal left-[30px] text-[#a5a5a5] text-[16px] top-[61px] w-[467px]">Добавьте новый расходник в каталог</p>
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold left-[30px] text-[20px] text-nowrap text-white top-[29px] whitespace-pre">Новый расходник</p>
+    </div>
+  );
+}
+
+function Group28() {
+  return (
+    <div className="absolute contents left-[30px] top-[245px]">
+      <div className="absolute bg-[#121212] h-[48px] left-[30px] rounded-[11px] top-[245px] w-[262px]">
+        <div aria-hidden="true" className="absolute border-2 border-[#8347eb] border-solid inset-0 pointer-events-none rounded-[11px]" />
+      </div>
+      <div className="absolute left-[256px] size-[24px] top-[258px]" data-name="gridicons:dropdown">
+        <div className="absolute inset-[41.67%_29.17%_37.5%_29.17%]" data-name="Vector">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 10 5">
+            <path d="M0 0L5 5L10 0H0Z" fill="var(--fill-0, #A5A5A5)" id="Vector" />
+          </svg>
+        </div>
+      </div>
+      <p className="absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[47.19px] not-italic text-[#a5a5a5] text-[17px] top-[259px] w-[226.581px]">Ароматическое масло</p>
+    </div>
+  );
+}
+
+function Group29() {
+  return (
+    <div className="absolute contents left-[310px] top-[246px]">
+      <div className="absolute bg-[#121212] h-[48px] left-[310px] rounded-[11px] top-[246px] w-[262px]">
+        <div aria-hidden="true" className="absolute border-2 border-[#292929] border-solid inset-0 pointer-events-none rounded-[11px]" />
+      </div>
+      <div className="absolute left-[536px] size-[24px] top-[259px]" data-name="gridicons:dropdown">
+        <div className="absolute inset-[41.67%_29.17%_37.5%_29.17%]" data-name="Vector">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 10 5">
+            <path d="M0 0L5 5L10 0H0Z" fill="var(--fill-0, #A5A5A5)" id="Vector" />
+          </svg>
+        </div>
+      </div>
+      <p className="absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[329px] not-italic text-[#a5a5a5] text-[17px] top-[260px] w-[160px]">штуки</p>
+    </div>
+  );
+}
+
+function Group35() {
+  return (
+    <div className="absolute contents left-[30px] top-[212px]">
+      <Group28 />
+      <Group29 />
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[normal] left-[30px] not-italic text-[17px] text-nowrap text-white top-[212px] whitespace-pre">Категория</p>
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[normal] left-[310px] not-italic text-[17px] text-nowrap text-white top-[212px] whitespace-pre">Единица измерения</p>
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <div className="absolute contents left-[306px] top-[695px]" data-name="Button">
+      <div className="absolute bg-[#8347eb] h-[48px] left-[306px] rounded-[12px] top-[695px] w-[264.905px]" />
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold h-[18px] leading-[normal] left-[calc(50%+138px)] not-italic text-[16px] text-center text-white top-[calc(50%+323.5px)] translate-x-[-50%] w-[120px]">Добавить</p>
+    </div>
+  );
+}
+
+function Button1() {
+  return (
+    <div className="absolute contents left-[29px] top-[695px]" data-name="Button">
+      <div className="absolute bg-[#121212] h-[48px] left-[29px] rounded-[12px] top-[695px] w-[264.905px]">
+        <div aria-hidden="true" className="absolute border border-[#292929] border-solid inset-0 pointer-events-none rounded-[12px]" />
+      </div>
+      <p className="absolute font-['Inter:Semi_Bold',sans-serif] font-semibold h-[18px] leading-[normal] left-[calc(50%-139px)] not-italic text-[16px] text-center text-white top-[calc(50%+323.5px)] translate-x-[-50%] w-[120px]">Отмена</p>
+    </div>
+  );
+}
+
+function Group38() {
+  return (
+    <div className="absolute contents left-[29px] top-[695px]">
+      <Button />
+      <Button1 />
+    </div>
+  );
+}
+
+export default function Group39() {
+  return (
+    <div className="relative size-full">
+      <div className="absolute bg-[#121212] h-[773px] left-0 rounded-[12px] top-0 w-[600px]">
+        <div aria-hidden="true" className="absolute border border-[#333333] border-solid inset-0 pointer-events-none rounded-[12px]" />
+      </div>
+      <Group34 />
+      <Group33 />
+      <Group32 />
+      <Group36 />
+      <Group37 />
+      <Group35 />
+      <Group38 />
+    </div>
+  );
+}
