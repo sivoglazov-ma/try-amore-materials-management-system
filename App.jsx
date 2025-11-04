987517a0fd794cf67febf44a1c84095e886f9657
sivoglazov-ma <sivoglazov.m@gmail.com>
2025-11-04 21:05:27 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Ваше приложение</h1>
+      {/* Ваш существующий JSX из App.tsx */}
+    </div>
+  );
+}
+
+export default App;
